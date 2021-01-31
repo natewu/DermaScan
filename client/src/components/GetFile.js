11c88bbd24957911eb651ea0c83b1805d8107da1
natewu <36091727+natewu@users.noreply.github.com>
2021-01-31 00:57:53 -0700

    formatting

--- client/src/components/GetFile.js
+++ client/src/components/GetFile.js
@@ -12,47 +12,47 @@ class GetFile extends React.Component {
         this.fileInput = React.createRef();
     }
 
-  handleFile(e) {
-      console.log("Uploading....");
-      this.state.file = e.target.files[0];
-      let fileName = this.fileInput.current.files[0].name;
-      console.log(fileName);
-      this.handleUpload(fileName);
-  }
+    handleFile(e) {
+        console.log("Uploading....");
+        this.state.file = e.target.files[0];
+        let fileName = this.fileInput.current.files[0].name;
+        console.log(fileName);
+        this.handleUpload(fileName);
+    }
 
-  async handleUpload(fileName) {
-    let formData = new FormData();
-    formData.append('file', this.state.file);
-    formData.append('name', 'skin pic');
-    let url = 'http://localhost:3005/image-uploads';
-    axios({
-        url: url,
-        method: "POST",
-        headers: {
-          "Content-Type": "multipart/form-data",
-         },
-        data:formData,
-        withCredentials: false,
-    })
-    .then((res) => {
-        if (res.status === 200) {
-            let filePath = url + '/' + fileName;
-            this.setImage(filePath);
-            console.log(this.state.results);
-        }
-        else {
-          console.log("Error occurred")
-        }
-    })
-    .catch((err) => { });
-  }
-  async setImage(path){
-  axios.get(path, { responseType: 'arraybuffer'})
-    .then(response => {
-        new Buffer(response.data, 'binary').toString('utf-8');
-        console.log(response);
-    })
-   }
+    async handleUpload(fileName) {
+        let formData = new FormData();
+        formData.append('file', this.state.file);
+        formData.append('name', 'skin pic');
+        let url = 'http://localhost:3005/image-uploads';
+        axios({
+            url: url,
+            method: "POST",
+            headers: {
+            "Content-Type": "multipart/form-data",
+            },
+            data:formData,
+            withCredentials: false,
+        })
+        .then((res) => {
+            if (res.status === 200) {
+                let filePath = url + '/' + fileName;
+                this.setImage(filePath);
+                console.log(this.state.results);
+            }
+            else {
+            console.log("Error occurred")
+            }
+        })
+        .catch((err) => { });
+    }
+    async setImage(path){
+        axios.get(path, { responseType: 'arraybuffer'})
+        .then(response => {
+            new Buffer(response.data, 'binary').toString('utf-8');
+            console.log(response);
+        })
+    }
 
     render() {
         return (
