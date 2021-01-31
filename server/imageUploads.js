504bb6b170372a6fe60a4fd8343bca564e7f09c5
Olivia L <oleonard5784@gmail.com>
2021-01-31 12:25:36 -0500

    fixing tensorflow

--- server/imageUploads.js
+++ server/imageUploads.js
@@ -12,11 +12,14 @@ const storage = multer.diskStorage({
 });
 
 const upload = multer({ storage });
+let image;
+const model = load();
 
 router.use(function(req, res, next){
   res.header("Access-Control-Allow-Origin", "*");
   res.header('Access-Control-Allow-Credentials', false);
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  image = req.file;
   next();
 });
 
@@ -27,44 +30,45 @@ router.get('/:image', function(req, res){
 router.post('/', upload.single('file'), (req, res) => {
   console.log("in router post")
 
-  return new Promise(() => {
-    const handler = tfn.io.fileSystem('./tfjs_model/model.json');
-  
-    return handler;
-  
-  }).then((yourHandler) => {
-    return tf.loadLayersModel(handler);
-  }).catch((error) => {
-    console.log(error) // here u can check an error
-  })
- 
+  // return new Promise(() => {
+  //   const handler = tfn.io.fileSystem('./tfjs_model/model.json');
+  //
+  //   return handler;
+  //
+  // }).then((myhandler) => {
+  //   return tf.loadLayersModel(handler);
+  // }).catch((error) => {
+  //   console.log(error) // here u can check an error
+  // })
+   console.log(predict(model));
+   res.send(predict(model));
+  // res.send("success");
+
 });
-let model;
+
 async function load() {
   const handler = tfn.io.fileSystem('./tfjs_model/model.json');
-  model = await tf.loadLayersModel("./tfjs_model/group1-shard5of5.bin");
+  let model = await tf.loadLayersModel("./tfjs_model/group1-shard5of5.bin");
   console.log("here");
   console.log(model);
-  res.send(predict(model));
-  res.send("success");
   return model;
-  
+
 };
 
 function predict(model) {
-  // code to connect to the <input> given value will go here (just not yet)
-  const image = req.file;
+  // code to connect to the <input> given value will go here (just not yet);
   console.log("here");
-  const image_tensor = tf.convert_to_tensor(image)  // and convert it to a tensor
+  // const image_tensor = tf.convert_to_tensor(image)  // and convert it to a tensor
   const verbose = true;
-
-  console.log(image_tensor.print(verbose));
-  const reshaped_tensor = tf.reshape([null,28,28,3]);
-  console.log(reshaped_tensor.print(verbose));
+  //
+  // console.log(image_tensor.print(verbose));
+  // let reshaped_tensor;
+  // reshaped_tensor = tf.reshape([224,224,3]);
+  //console.log(reshaped_tensor.print(verbose));
 
   // now lets make the prediction
   model.then(model => {
-    let result = model.predict(reshapedTensor);
+    let result = model.predict(image);
     console.log(result);
     // result = result.round().dataSync()[0];  // round and get value
     // alert(result ? "odd" : "even");  // creates pop-up
