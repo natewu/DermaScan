477171ae0e89cb5cfad68171d89a1243eae82c06
Olivia L <oleonard5784@gmail.com>
2021-01-31 07:40:58 -0500

    merge ml and app

--- server/imageUploads.js
+++ server/imageUploads.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const multer = require('multer');
+const tf = require("@tensorflow/tfjs");
+const tfn = require("@tensorflow/tfjs-node");
 
 const storage = multer.diskStorage({
   destination: './image-uploads',
@@ -20,9 +22,57 @@ router.use(function(req, res, next){
 
 router.get('/:image', function(req, res){
     res.sendFile(__dirname + '/image-uploads' + req.path);
-    //res.send(req);
 });
 router.post('/', upload.single('file'), (req, res) => {
-    res.send("success");
-});
+  console.log("")
+  // return new Promise(() => {
+  //   const handler;
+
+  //   return handler;
+
+  // }).then(tfn.io.fileSystem('./tfjs_model/model.json') => {
+  //   return tf.loadLayers(tfn.io.fileSystem('./tfjs_model/model.json'))
+  // }).catch((error) => {
+  //   console.log(error) // here u can check an error
+  // })
+
+  async function load() {
+    const handler = tfn.io.fileSystem('./tfjs_model/model.json');
+    var model = await tf.loadLayersModel(handler);
+    console.log("here");
+    console.log(model);
+    return model;
+  };
+  model = load();
+
+
+// const handler = tfn.io.fileSystem('./tfjs_model/model.json');
+// const model = await tf.loadModel(handler);
+
+  function predict(model) {
+    // code to connect to the <input> given value will go here (just not yet)
+
+    const image = req.file;
+    console.log("here");
+    const image_tensor = tf.convert_to_tensor(image)  // and convert it to a tensor
+    const verbose = true;
+
+    console.log(image_tensor.print(verbose));
+    const reshaped_tensor = tf.reshape([null,28,28,3]);
+    console.log(reshaped_tensor.print(verbose));
+
+    // now lets make the prediction
+    model.then(model => {
+      let result = model.predict(reshapedTensor);
+      console.log(result);
+      // result = result.round().dataSync()[0];  // round and get value
+      // alert(result ? "odd" : "even");  // creates pop-up
+    });
+  };
+
+
+      console.log(req.file);
+      res.send(req.file);
+  });
+
 module.exports = router;
